ff2618a989d2b3228e57547f24ad39eafa76511e
小白 <3104026189@qq.com>
2023-08-29 10:47:05 +0800

    Support for using HuskSync API instead of Bukkit#getOfflinePlayers

--- syncable-bukkit/src/main/java/dev/rgbmc/syncable/migrators/HuskSyncMigrator.java
+++ syncable-bukkit/src/main/java/dev/rgbmc/syncable/migrators/HuskSyncMigrator.java
@@ -6,69 +6,99 @@ import dev.rgbmc.syncable.SyncableBukkit;
 import dev.rgbmc.syncable.client.migrators.MigrateWriteHandler;
 import dev.rgbmc.syncable.client.migrators.Migrator;
 import dev.rgbmc.syncable.utils.ItemSerializer;
-import java.util.Optional;
+import net.william278.husksync.BukkitHuskSync;
 import net.william278.husksync.api.HuskSyncAPI;
+import net.william278.husksync.config.Settings;
 import net.william278.husksync.data.BukkitInventoryMap;
+import net.william278.husksync.database.MySqlDatabase;
 import net.william278.husksync.player.User;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.*;
+
 public class HuskSyncMigrator extends Migrator {
+
+  private Collection<User> getAllHuskSyncUser() {
+    MySqlDatabase database = (MySqlDatabase) BukkitHuskSync.getInstance().getDatabase();
+
+    Class<?> clazz = database.getClass();
+    try {
+      Method method = clazz.getDeclaredMethod("getConnection");
+      Connection connection = (Connection) method.invoke(database);
+      String table = BukkitHuskSync.getInstance().getSettings().getTableName(Settings.TableName.USERS);
+
+      List<User> userList = new ArrayList<>();
+
+      try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM " + table)) {
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+          UUID uuid = UUID.fromString(resultSet.getString("uuid"));
+          String name = resultSet.getString("username");
+          userList.add(new User(uuid, name));
+        }
+        resultSet.close();
+        return userList;
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
   @Override
   public void migrate() {
     HuskSyncAPI api = HuskSyncAPI.getInstance();
-    for (OfflinePlayer player : Bukkit.getOfflinePlayers()) {
+
+    for (User user : getAllHuskSyncUser()) {
       JsonObject jsonObject = new JsonObject();
-      api.getUser(player.getUniqueId())
+
+      api.getPlayerInventory(user)
           .thenAcceptAsync(
-              optionalUser -> {
-                if (!optionalUser.isPresent()) return;
-                User user = optionalUser.get();
-                api.getPlayerInventory(user)
+              optionalBukkitInventoryMap -> {
+                // Migrate Inventory
+                if (optionalBukkitInventoryMap.isPresent()) {
+                  JsonObject inventoryJson = new JsonObject();
+                  BukkitInventoryMap inventoryMap = optionalBukkitInventoryMap.get();
+                  for (int i = 0; i < inventoryMap.getSize(); i++) {
+                    Optional<ItemStack> optionalItem = inventoryMap.getItemAt(i);
+                    if (optionalItem.isPresent()) {
+                      inventoryJson.addProperty(
+                          String.valueOf(i),
+                          ItemSerializer.serialize(optionalItem.get()));
+                    } else {
+                      inventoryJson.addProperty(
+                          String.valueOf(i),
+                          ItemSerializer.serialize(new ItemStack(Material.AIR)));
+                    }
+                  }
+                  jsonObject.addProperty("inventory", new Gson().toJson(inventoryJson));
+                }
+                api.getPlayerEnderChest(user)
                     .thenAcceptAsync(
-                        optionalBukkitInventoryMap -> {
-                          // Migrate Inventory
-                          if (optionalBukkitInventoryMap.isPresent()) {
-                            JsonObject inventoryJson = new JsonObject();
-                            BukkitInventoryMap inventoryMap = optionalBukkitInventoryMap.get();
-                            for (int i = 0; i < inventoryMap.getSize(); i++) {
-                              Optional<ItemStack> optionalItem = inventoryMap.getItemAt(i);
-                              if (optionalItem.isPresent()) {
-                                inventoryJson.addProperty(
-                                    String.valueOf(i),
-                                    ItemSerializer.serialize(optionalItem.get()));
-                              } else {
-                                inventoryJson.addProperty(
-                                    String.valueOf(i),
-                                    ItemSerializer.serialize(new ItemStack(Material.AIR)));
-                              }
+                        optionalItemStacks -> {
+                          // Migrate EnderChest
+                          if (optionalItemStacks.isPresent()) {
+                            JsonObject enderChestJson = new JsonObject();
+                            ItemStack[] itemStacks = optionalItemStacks.get();
+                            for (int i = 0; i < itemStacks.length; i++) {
+                              enderChestJson.addProperty(
+                                  String.valueOf(i),
+                                  ItemSerializer.serialize(itemStacks[i]));
                             }
-                            jsonObject.addProperty("inventory", new Gson().toJson(inventoryJson));
+                            jsonObject.addProperty(
+                                "enderChest", new Gson().toJson(enderChestJson));
                           }
-                          api.getPlayerEnderChest(user)
-                              .thenAcceptAsync(
-                                  optionalItemStacks -> {
-                                    // Migrate EnderChest
-                                    if (optionalItemStacks.isPresent()) {
-                                      JsonObject enderChestJson = new JsonObject();
-                                      ItemStack[] itemStacks = optionalItemStacks.get();
-                                      for (int i = 0; i < itemStacks.length; i++) {
-                                        enderChestJson.addProperty(
-                                            String.valueOf(i),
-                                            ItemSerializer.serialize(itemStacks[i]));
-                                      }
-                                      jsonObject.addProperty(
-                                          "enderChest", new Gson().toJson(enderChestJson));
-                                    }
-                                    SyncableBukkit.getSyncableClient()
-                                        .sendCommand(
-                                            new MigrateWriteHandler(
-                                                player.getUniqueId(), jsonObject));
-                                  });
+                          SyncableBukkit.getSyncableClient()
+                              .sendCommand(new MigrateWriteHandler(user.uuid, jsonObject));
                         });
               });
     }
+
   }
 }
