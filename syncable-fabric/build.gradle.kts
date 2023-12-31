bc60c543b9c61aabb6a4e25d80680fc3d1829a97
killerprojecte <admin@fastmcmirror.org>
2023-12-31 14:38:23 +0800

    Bump to 1.20.4

--- syncable-fabric/build.gradle.kts
+++ syncable-fabric/build.gradle.kts
@@ -11,10 +11,10 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.20.2")
-    mappings("net.fabricmc:yarn:1.20.2+build.1:v2")
-    modImplementation("net.fabricmc:fabric-loader:0.14.22")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.89.2+1.20.2")
+    minecraft("com.mojang:minecraft:1.20.4")
+    mappings("net.fabricmc:yarn:1.20.4+build.3:v2")
+    modImplementation("net.fabricmc:fabric-loader:0.15.3")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.92.0+1.20.4")
     include(implementation(project(":syncable-client"))!!)
     include(implementation("org.fastmcmirror:yaml:1.3.0")!!)
     include(implementation("org.yaml:snakeyaml:1.33")!!)
